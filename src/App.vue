146efcf2bbbb4f48bf8148e8a00ff60e832fc7f0
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2022-04-05 19:04:53 -0300

    feat: configuração de layout e vuex

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="text-center" id="loader" v-if="$wait.any">
+      <div class="spinner-border" style role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <p class="text-center">Aguarde...</p>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
+require("./bootstrap");
+
+
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueWait from "vue-wait";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+import router from "./routes";
+import store from "./store";
+
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter); //rotas
+Vue.use(VueWait);
+Vue.use(BootstrapVue); // Install BootstrapVue
+Vue.use(IconsPlugin); // Optionally install the BootstrapVue icon components plugin
+
+
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  components: { App },
+  data: {
+  },
+  router,
+  store,
+  wait: new VueWait(),
+  render: h => h(App)
+
+});
