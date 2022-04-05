c1a936419c7bb45f83009cde5280973ceffb5572
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2022-04-05 19:13:34 -0300

    feat: adiciona nome e imagem no service

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,11 +14,7 @@
                 Sale
               </div>
               <!-- Product image-->
-              <img
-                class="card-img-top"
-                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-                alt="..."
-              />
+              <img class="card-img-top" :src="item.image" alt="..." />
               <!-- Product details-->
               <div class="card-body p-4">
                 <div class="text-center">
